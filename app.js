b30b6232e387f171eedb08d8d85a8406745624d9
Daniel Saputra <danielwetan.io@gmail.com>
2020-09-09 21:17:16 +0700

    feature: MySQL connection

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
+const connection = require('./src/helpers/mysql');
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log('Database has connected');
+})
 
 app.get("/", (req, res) => {
   res.json("Hello world!")
