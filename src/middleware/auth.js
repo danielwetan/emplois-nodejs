d544ed1c76a90065d3c08f86492e12ee73ef212e
Daniel Saputra <danielwetan.io@gmail.com>
2020-09-11 03:54:40 +0700

    feature: JWT token middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const config = require('../configs/global');
+const helper = require('../helpers/response');
+
+module.exports = {
+  verifyJwtToken: (req, res, next) => {
+    let token = config.jwt.mainToken;
+    token = req.headers.authorization
+    try {
+      let decoded = jwt.verify(token, config.jwt.secretKey);
+      console.log(decoded);
+      next();
+    } catch (err) {
+      if (err.name === 'TokenExpiredError') {
+        return helper.response(res, 'fail', 'Token expired!', 401);
+      }
+      return helper.response(res, 'fail', 'Invalid token', 401);
+    }
+  },
+  checkRole: (req, res, next) => {
+    try {
+      const role = decoded.role
+      if (role == 1) {
+        next();
+      } else {
+        const msg = `You're not an admin!`
+        return helper.response(res, 'fail', msg, 404)
+      }
+    } catch {
+      return helper.response(res, 'fail', 'Something Error!!', 404);
+    }
+  }
+}
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,15 +2,17 @@ const express = require('express');
 const router = express.Router();
 const profileController = require('../controllers/profile');
 const multer = require('../helpers/multer');
+const authMiddleware = require('../middleware/auth');
+const verifyJwtToken = authMiddleware.verifyJwtToken
 const redis = require('../middleware/redis');
 
 router.get('/me/:user_id', redis.getCached('talent'), profileController.talent.getGeneralInfo);
 router.get('/portofolio/:user_id', profileController.talent.getPortofolio);
 router.get('/experience/:user_id', profileController.talent.getWorkExperience);
 
-router.patch('/me', multer.single('image'), profileController.talent.updateGeneralInfo);
-router.patch('/social', multer.none(), profileController.talent.updateSocialMedia);
-router.patch('/experience', multer.single('image'), profileController.talent.updateWorkExperience);
-router.patch('/portofolio', multer.single('image'), profileController.talent.updatePortofolio);
+router.patch('/me', verifyJwtToken, multer.single('image'), profileController.talent.updateGeneralInfo);
+router.patch('/social', verifyJwtToken, multer.none(), profileController.talent.updateSocialMedia);
+router.patch('/experience', verifyJwtToken, multer.single('image'), profileController.talent.updateWorkExperience);
+router.patch('/portofolio', verifyJwtToken, multer.single('image'), profileController.talent.updatePortofolio);
 
 module.exports = router;
